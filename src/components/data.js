4989a4fe3e68c3b85e14d50466e32fcf6a7308e8
Ishant Bhatt <ishantbhatt1654@gmail.com>
2024-03-31 23:19:10 +0530

    Changes

---
+++ src/components/data.js
@@ -0,0 +1,70 @@
+import cardImg1 from "../assets/img1.jpg";
+import cardImg2 from "../assets/img2.jpg";
+import cardImg3 from "../assets/img3.jpg";
+import cardImg4 from "../assets/img4.jpg";
+import cardImg5 from "../assets/img5.jpg";
+const data = [
+  {
+    id: 1,
+    title: "Las Vegas Aviators",
+    img: cardImg2,
+    date: "OCT 15 | SUN | 4:30 PM",
+    location: "Las Vegas Ballpark, Las Vegas, Nevada",
+    button: "Take Flight Collection",
+  },
+  {
+    id: 2,
+    title: "Sacramento River Cats",
+    img: cardImg1,
+    date: "OCT 22 | SUN | 4:30 PM",
+    location: "Sutter Health Park, Sacramento, California",
+    button: "Orange Collection",
+  },
+  {
+    id: 3,
+    title: "Las Vegas Aviators",
+    img: cardImg2,
+    date: "OCT 15 | SUN | 4:30 PM",
+    location: "Las Vegas Ballpark, Las Vegas, Nevada",
+    button: "Take Flight Collection",
+  },
+  {
+    id: 4,
+    title: "Sacramento River Cats",
+    img: cardImg1,
+    date: "OCT 22 | SUN | 4:30 PM",
+    location: "Sutter Health Park, Sacramento, California",
+    button: "Orange Collection",
+  },
+];
+const data2 = [
+  {
+    id: 1,
+    title: "Sacramento River Cats",
+    img: cardImg5,
+    Number: "48",
+    sport: "Baseball",
+  },
+  {
+    id: 2,
+    title: "Las Vegas Aviators",
+    img: cardImg3,
+    Number: "15",
+    sport: "Baseball",
+  },
+  {
+    id: 3,
+    title: "New Jersey Devils",
+    img: cardImg4,
+    Number: "28",
+    sport: "Baseball",
+  },
+  {
+    id: 4,
+    title: "Las Vegas Aviators",
+    img: cardImg3,
+    Number: "15",
+    sport: "Baseball",
+  },
+];
+export { data, data2 };
